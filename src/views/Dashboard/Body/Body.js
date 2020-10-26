5c2e03276793ee1baf7d372b176c31bd5eff0981
oliver <olivercastroteran@gmail.com>
2020-10-26 11:58:43 -0400

    header added

--- src/views/Dashboard/Body/Body.js
+++ src/views/Dashboard/Body/Body.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../../../components/Header/Header';
 import './Body.css';
 
-const Body = () => {
+const Body = ({ spotify }) => {
   return (
     <div className="body">
-      <h1>Body</h1>
+      <Header spotify={spotify} />
     </div>
   );
 };
